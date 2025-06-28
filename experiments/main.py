a9e93281f5946f9a2bea3766fe576a784851ea29
kai <kaiajmansfield1999@gmail.com>
2025-06-28 13:22:28 +0100

    1

--- experiments/main.py
+++ experiments/main.py
@@ -130,6 +130,7 @@ def main(args):
         del params_copy['device']
         wandb.init(project=args.wandb_project, name=exp_name, config=params_copy)
     
+    ckpt_path = f"{args.results_base_folder}/{args.dataset}/{args.model}/{exp_name}"
     if not os.path.exists(ckpt_path):
         if distributed_backend.is_master_process():
             os.makedirs(ckpt_path)
