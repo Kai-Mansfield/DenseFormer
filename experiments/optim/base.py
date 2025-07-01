e26d7ae2b272e2ca58f537c1c8188148c0778697
kai <kaiajmansfield1999@gmail.com>
2025-07-01 13:27:52 +0100

    1

--- experiments/optim/base.py
+++ experiments/optim/base.py
@@ -61,6 +61,9 @@ def train_base(model, opt, data, scheduler, iterations, acc_steps, batch_size, s
                 if getattr(model, "needs_iter", False):
                     outputs = model(x, targets=y, iter=itr)
                 else:
+                    device = next(model.parameters()).device 
+                    x = x.to(device)
+                    y = y.to(device)
                     print('x device:', x.device)
                     print('y device:', y.device)
                     print('model params device:', next(model.parameters()).device)
