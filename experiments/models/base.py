97fc8451dc93b0e7e914ff383d22e6f50e5ca224
kai <kaiajmansfield1999@gmail.com>
2025-08-24 19:30:36 +0100

    1

--- experiments/models/base.py
+++ experiments/models/base.py
@@ -612,7 +612,7 @@ class GPTBase(nn.Module):
             # grad_before = x.grad  # might be None if no backward yet
 
             # # Now, move the module to cuda:1
-            targets = safe_move(targets, "cuda:0")
+            #targets = safe_move(targets, "cuda:0")
 
             # Get the embedding weights after transfer
             wte_after = targets.data.cpu().clone()
