ef259eba66828afdd2aa622b6616fb9071d0c12a
kai <kaiajmansfield1999@gmail.com>
2025-08-27 09:21:03 +0100

    1

--- experiments/optim/base.py
+++ experiments/optim/base.py
@@ -103,11 +103,19 @@ def train_base(model, opt, data, scheduler, iterations, acc_steps, batch_size, s
                     print(f"Inf detected in parameter be4 loss: {name}")
             # print('finished cheking for inf and nans in parameters be4 loss')
             loss.backward()
+            total, updated = 0, 0
             for i, (name, p) in enumerate(model.named_parameters()):
                 if torch.isnan(p).any():
                     print(f"NaN detected in parameter after loss: {name}")
                 if torch.isinf(p).any():
                     print(f"Inf detected in parameter after loss: {name}")
+                if p.requires_grad:
+                    total += 1
+                    has_grad = (p.grad is not None) and torch.isfinite(p.grad).all() and (p.grad.abs().sum() > 0)
+                    updated += int(has_grad)
+                    if not has_grad:
+                        print("NO GRAD:", name, p.device, p.shape)
+            print(f"grad-bearing params with nonzero grads: {updated}/{total}")
             #print('finished cheking for inf and nans in parameters after loss')
             substep += 1
 
