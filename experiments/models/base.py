ca21b033610e621db24ea60f62e2d4bf55e249f0
kai <kaiajmansfield1999@gmail.com>
2025-09-16 11:09:48 +0100

    1

--- experiments/models/base.py
+++ experiments/models/base.py
@@ -237,12 +237,12 @@ class GPTBase(nn.Module):
         
         n_layer = config.n_layer
 
-        if n_layer <= 22:
+        if n_layer <= 21:
             # All go to cuda:1
             self.n_cuda0 = 0
         else:
             # First 12 fixed on cuda:1
-            remaining = n_layer - 22
+            remaining = n_layer - 21
             self.n_cuda0 = (remaining + 1) // 2  
 
         # Now move layers
