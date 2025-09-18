41e97506abe4f344edce817b06f35b6173dfdd0f
kai <kaiajmansfield1999@gmail.com>
2025-09-18 08:27:42 +0100

    1

--- experiments/models/denseformer2.py
+++ experiments/models/denseformer2.py
@@ -334,6 +334,7 @@ class DenseFormer2(nn.Module):
                 
             )
             x_accs[rep_idx % self.dilation_factor] = (safe_move(full_tensor, "cuda:1"), safe_move(new_slice, "cuda:1"))
+            print('x_accs[0]', x_accs[0])
             print('full_tensor.device, new_slice.devic', safe_move(full_tensor, "cuda:1").device, safe_move(new_slice, "cuda:1").device)
             print('x_accs[rep_idx % self.dilation_factor][0].dev', x_accs[rep_idx % self.dilation_factor][0].device)
             print('x_accs[rep_idx % self.dilation_factor][1].dev', x_accs[rep_idx % self.dilation_factor][1].device)
