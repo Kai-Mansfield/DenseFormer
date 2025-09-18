71a7f6b342661c9612e512455d579b01eadf2015
kai <kaiajmansfield1999@gmail.com>
2025-09-18 08:17:10 +0100

    1

--- experiments/models/denseformer2.py
+++ experiments/models/denseformer2.py
@@ -336,6 +336,7 @@ class DenseFormer2(nn.Module):
             print('safe_move(full_tensor, "cuda:1").device, safe_move(new_slice, "cuda:1").devic', safe_move(full_tensor, "cuda:1").device, safe_move(new_slice, "cuda:1").device)
             print('x_accs[rep_idx % self.dilation_factor][1].dev', x_accs[rep_idx % self.dilation_factor][1].device)
             x_stack = x_accs[rep_idx % self.dilation_factor][1] 
+            print(x_stack.device)
             if x_stack is None:
                 raise RuntimeError(f"x_stack is None at rep_idx={rep_idx}")
             C = x_stack.shape[-1]
