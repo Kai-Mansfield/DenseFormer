4b3dcbd0c53d4923bdd989ed7ae527bc697ea39d
kai <kaiajmansfield1999@gmail.com>
2025-09-18 07:40:33 +0100

    1

--- experiments/models/denseformer2.py
+++ experiments/models/denseformer2.py
@@ -325,7 +325,6 @@ class DenseFormer2(nn.Module):
             if rep_idx == 1 + self.n_cuda0:
                 x = safe_move(x, "cuda:1")
                 print('len x_accs[rep_idx % self.dilation_factor]', len(x_accs[rep_idx % self.dilation_factor]))
-                x_accs[rep_idx % self.dilation_factor][1] = safe_move(x_accs[rep_idx % self.dilation_factor][1], "cuda:1")
             for block in self.transformer.h[rep_idx-1]:
                 x = block(x, pos_emb_closure, cache_context, start_index=index_shift)
             x_accs[rep_idx % self.dilation_factor] = apply_inplace_set(
