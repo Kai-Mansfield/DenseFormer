410ba78f12167b37813a7da783508a1a28dd8769
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-10-31 23:29:30 +0000

    1

--- experiments/main.py
+++ experiments/main.py
@@ -53,9 +53,10 @@ def adjust_state_dict(state_dict, model):
     return state_dict
 
 def main(args): 
+    torch.use_deterministic_algorithms(True)
+    torch.backends.cudnn.benchmark = False
     torch.backends.cuda.matmul.allow_tf32 = False
     torch.backends.cudnn.allow_tf32 = False
-    torch.use_deterministic_algorithms(True)
 
     distributed_backend = distributed.make_backend_from_args(args)
     print(f"Using backend: {type(distributed_backend)}")
