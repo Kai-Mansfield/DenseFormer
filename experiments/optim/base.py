006bc2522957df450aeb34ff0f21549af4fc077c
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-11-01 00:19:45 +0000

    1

--- experiments/optim/base.py
+++ experiments/optim/base.py
@@ -109,12 +109,12 @@ def train_base(model, opt, data, scheduler, iterations, acc_steps, batch_size, s
                 t0 = time.time()
         if True:
             if extra_args.save_checkpoint_freq is not None and itr % extra_args.save_checkpoint_freq == 0:
-                print(f"saving checkpoint to {ckpt_path}/{extra_args.ckpt_name}")
                 save_checkpoint(distributed_backend=distributed_backend,
                                 model=model,
                                 opt=opt,
                                 scheduler=scheduler,
                                 itr=itr,
                                 ckpt_path=f"{ckpt_path}/{extra_args.ckpt_name}")
+                print(f"saved checkpoint to {ckpt_path}/{extra_args.ckpt_name}")
 
     return stats
