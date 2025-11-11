5ef764a6bd32dfa2e56b9b2526eca5a728a72b87
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-11-11 19:05:29 +0000

    1

--- experiments/main.py
+++ experiments/main.py
@@ -148,7 +148,7 @@ def main(args):
                 # Return scaling factor relative to base LR
                 return lr / max_lr
 
-            scheduler = torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda=lr_lambda)
+            scheduler = torch.optim.lr_scheduler.LambdaLR(opt, lr_lambda=lr_lambda, last_epoch=args.start_iter)
 
         elif args.scheduler == 'linear':
             # Linear decay with warmup
