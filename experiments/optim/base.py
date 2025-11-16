56a27b1028c57a34a70ea0287a71c8cf718e0131
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-11-16 22:19:27 +0000

    1

--- experiments/optim/base.py
+++ experiments/optim/base.py
@@ -146,7 +146,7 @@ def train_base(model, opt, data, scheduler, iterations, acc_steps, batch_size, s
                                 scheduler=scheduler,
                                 itr=itr,
                                 ckpt_path=f"{ckpt_path}/{extra_args.ckpt_name}")
-                with open(f"{ckpt_path}/{extra_args.ckpt_name}_grad_norms.txt", "w") as f:
+                with open(f"{ckpt_path}/{extra_args.ckpt_name}_grad_norms.txt", "a") as f:
                     for g in gn:
                         f.write(f"{g}\n")
                 gn = []
