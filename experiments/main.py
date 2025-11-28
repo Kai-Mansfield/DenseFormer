d27bf346a7d5672923a9585d9417329d96944d09
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-11-28 12:17:26 +0000

    1

--- experiments/main.py
+++ experiments/main.py
@@ -98,7 +98,10 @@ def main(args):
 
     if args.dlr is None:
         args.dlr = args.lr
-    group_specs = distributed_backend.get_raw_model(model).get_parameter_group_specs()
+    if args.model == 'denseformer2':
+       group_specs = distributed_backend.get_raw_model(model).get_parameter_group_specs(args.dlr, dense_weight_decay=args.weight_decay) 
+    else:
+        group_specs = distributed_backend.get_raw_model(model).get_parameter_group_specs()
     param_name_mapping = {p_name: p for p_name, p in model.named_parameters()}
     optimized_params_cnt = 0
 
