ab744dfb3289e355eac97cc4e9d24a47cc694c5e
Kai Mansfield <kaiajmansfield1999@gmail.com>
2025-12-04 06:51:40 +0000

    1

--- experiments/data/openwebtext2.py
+++ experiments/data/openwebtext2.py
@@ -49,7 +49,7 @@ def get_openwebtext2_data(config):
         # === Create train / val / test splits ===
         # First split into (train+val) and test
         split_1 = dataset.train_test_split(test_size=0.0005, shuffle=True, seed=2357)
-        test_dataset = split_1["val"]
+        val_dataset = split_1["val"]
 
         # Now split (train+val)
         # split_2 = split_1["train"].train_test_split(test_size=0.0005, shuffle=True)
